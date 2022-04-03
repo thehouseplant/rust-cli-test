39ec51019488542d8df62e423a843aeed48356eb
thehouseplant <sean.collins@outlook.com>
2022-04-03 16:56:05 -0700

    Add basic command line parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+struct Cli {
+    pattern: String,
+    path: std::path::PathBuf,
+}
+
 fn main() {
+    let pattern = std::env::args().nth(1).expect("No pattern given");
+    let path = std::env::args().nth(2).expect("No path given");
+
+    let args = Cli {
+        pattern: pattern,
+        path: std::path::PathBuf::from(path),
+    };
+
     println!("Hello, world!");
 }
